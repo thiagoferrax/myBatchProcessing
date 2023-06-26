55f57203df08419fdbb41f43b1d6ddfb85da237a
thiagoferrax <thiagoferrax@gmail.com>
2023-06-26 21:25:22 +0100

    Initial code from spring initializr.

---
+++ src/main/java/com/thiagoferraz/myBatchProcessing/MyBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoferraz.myBatchProcessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatchProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagoferraz/myBatchProcessing/MyBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoferraz.myBatchProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
